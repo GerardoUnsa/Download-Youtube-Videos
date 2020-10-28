62cfa1a5832287a45bc579e89ad5eba97b51fbc2
GerardoTBO <gersrpb@gmail.com>
2020-10-28 09:47:03 -0500

    cambios

--- script.py
+++ script.py
@@ -3,6 +3,8 @@ from pytube import YouTube
 link = 'https://www.youtube.com/watch?v=oygrmJFKYZY' # Dua Lipa Song
 yt = YouTube(link)
 print("Title: ",yt.title) # Title video
+print("Descripcion: ",yt.description) # Title description
+print("Duracion: ",yt.length) # Title description
 
 yt.streams.filter('mp4')
 yt.streams.first().download()
